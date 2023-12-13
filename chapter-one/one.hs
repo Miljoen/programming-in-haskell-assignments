37cba534463187cd07003d7ff74669743a4719d9
Yoeri Moens <yoerimoens@gmail.com>
2023-12-13 12:58:01 +0100

    Chapter 1, assignment 1

---
+++ chapter-one/one.hs
@@ -0,0 +1,25 @@
+-- 1. Give another possible calculation for the result of double (double 2).
+
+-- double (double 2)
+-- applying the inner double
+-- double (2 + 2) 
+-- applying +
+-- double 4
+-- applying double
+-- 4 + 4
+-- applying +
+-- 8
+
+-- Alternative way
+
+-- double (double 2)
+-- applying the outer double
+-- double 2 + double 2
+-- applying the first double
+-- (2 + 2) + double 2
+-- applying the second double
+-- (2 + 2) + (2 + 2)
+-- applying the inner +
+-- 4 + 4
+-- applying +
+-- 8
